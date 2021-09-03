bf655406cd1cf9e7444d9922b99dbf4b4d05d672
YL <simon-ling@hotmail.com>
2021-09-02 22:48:49 -0400

    4245 is done

--- 03-setup/setup.test.js
+++ 03-setup/setup.test.js
@@ -1,17 +1,33 @@
 let animals = ["elephant", "zebra", "bear", "tiger"];
-describe("animals array", () => {
+beforeAll(() => {
+  console.log("before all tests");
   animals = ["elephant", "zebra", "bear", "tiger"];
+});
+beforeEach(() => {
+  console.log("before each test");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+afterEach(() => {
+  console.log("after each test");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+afterAll(() => {
+  console.log("after all tests");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+describe("animals array", () => {
+  //   animals = ["elephant", "zebra", "bear", "tiger"];
   test("should add animal to the end of array", () => {
     animals.push("aligator");
     expect(animals[animals.length - 1]).toBe("aligator");
   });
   test("should add animal to the beginning of array", () => {
-    animals = ["elephant", "zebra", "bear", "tiger"];
+    // animals = ["elephant", "zebra", "bear", "tiger"];
     animals.unshift("monkey");
     expect(animals[0]).toBe("monkey");
   });
   test("should have initial length of 4", () => {
-    animals = ["elephant", "zebra", "bear", "tiger"];
+    // animals = ["elephant", "zebra", "bear", "tiger"];
     expect(animals.length).toBe(4);
   });
 });
