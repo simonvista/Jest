df90c28b855241a27c26c21c89c3ddf6a2a4dfb2
YL <simon-ling@hotmail.com>
2021-09-02 22:54:46 -0400

    4705 is done

--- 03-setup/setup.test.js
+++ 03-setup/setup.test.js
@@ -1,20 +1,20 @@
 let animals = ["elephant", "zebra", "bear", "tiger"];
-beforeAll(() => {
-  console.log("before all tests");
-  animals = ["elephant", "zebra", "bear", "tiger"];
-});
+// beforeAll(() => {
+//   console.log("before all tests");
+//   animals = ["elephant", "zebra", "bear", "tiger"];
+// });
 beforeEach(() => {
   console.log("before each test");
   animals = ["elephant", "zebra", "bear", "tiger"];
 });
-afterEach(() => {
-  console.log("after each test");
-  animals = ["elephant", "zebra", "bear", "tiger"];
-});
-afterAll(() => {
-  console.log("after all tests");
-  animals = ["elephant", "zebra", "bear", "tiger"];
-});
+// afterEach(() => {
+//   console.log("after each test");
+//   animals = ["elephant", "zebra", "bear", "tiger"];
+// });
+// afterAll(() => {
+//   console.log("after all tests");
+//   animals = ["elephant", "zebra", "bear", "tiger"];
+// });
 describe("animals array", () => {
   //   animals = ["elephant", "zebra", "bear", "tiger"];
   test("should add animal to the end of array", () => {
@@ -29,5 +29,10 @@ describe("animals array", () => {
   test("should have initial length of 4", () => {
     // animals = ["elephant", "zebra", "bear", "tiger"];
     expect(animals.length).toBe(4);
+  });
+});
+describe("test something else", () => {
+  test("true should be truthy", () => {
+    expect(true).toBeTruthy();
   });
 });
