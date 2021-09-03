c32060d9e56a7abc5656a3dba4f57b1bd034715f
YL <simon-ling@hotmail.com>
2021-09-02 20:23:29 -0400

    1135 is done

--- sum.test.js
+++ sum.test.js
@@ -2,7 +2,7 @@ const sum = require("./sum");
 
 // test or it
 //group test blocks
-describe("group tests", () => {
+describe("tests group", () => {
   it("test sum()", () => {
     const res = sum(1, 2);
     expect(res).toBe(3);
@@ -10,5 +10,33 @@ describe("group tests", () => {
   test("test obj", () => {
     const obj = {};
     expect(obj).toEqual({});
+  });
+});
+// in JS, if("") | if(null) | if(undefined) | if(0) -> falsey, if("sdfg5") -> truthy
+describe("truthy or falsy #1", () => {
+  it("null", () => {
+    const n = null; //null is not undefined!!!
+    // multi-expects
+    expect(n).toBeFalsy();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeNull();
+    expect(n).not.toBeUndefined();
+  });
+});
+describe("truthy or falsy #2", () => {
+  it("null", () => {
+    const n = 0; //0 is defined!!!
+    // multi-expects
+    expect(n).toBeFalsy();
+    expect(n).not.toBeTruthy();
+  });
+});
+describe("truthy or falsy #3", () => {
+  it("null", () => {
+    const n = ""; //"" is defined!!!
+    // multi-expects
+    expect(n).toBeFalsy();
+    expect(n).not.toBeTruthy();
+    expect(n).not.toBeUndefined();
   });
 });
